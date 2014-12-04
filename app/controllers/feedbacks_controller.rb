84cb22e41b34dc94e81b4a18e605b9ba0c2c22a1
dathi <dathi@tma.com.vn>
2014-12-04 10:45:16 +0700

    update css, add debug

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -13,12 +13,19 @@ class FeedbacksController < ApplicationController
 
   def create
     @feedback = Feedback.new(params[:feedback])
+    p '------1. create new'
+    p @feedback
     # hthngoc - Captcha
     # Double-check at server side for security
     captcha = "https://www.google.com/recaptcha/api/siteverify?secret=6LcAov4SAAAAABDAy4U-_rXL_1bDx12QrG0-i-LB&response=#{params["g-recaptcha-response"]}"
     response_m = open(captcha).read
+    p '-----2. captcha & respone' 
+    p captcha
+    p response_m  
     if response_m['true']
+      p '-----3. captcha is valid' 
       if @feedback.valid?
+        p '-----4. sending email'
         # dathi - Send email
         begin
           UserMailer.feedback(params[:feedback])
@@ -28,10 +35,11 @@ class FeedbacksController < ApplicationController
           flash[:error] = "There is some error. Sorry for this inconvenience."
         end
         redirect_to root_url
-      else
+      else        
         render :action => 'new'
       end
     else
+      p '-----5: captcha is not valid, do nothing' 
       flash[:error] = "Oh! It's error with CAPTCHA! Are you a human?"
       render :action => 'new'
     end
